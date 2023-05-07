c2329ba4362aa2dad9822da799429f7e4415197c
Golden_Water <2439577268@qq.com>
2023-05-07 21:29:49 +0800

    feat(ui): localize

--- tools_web_ui/src/app/page/Entry.tsx
+++ tools_web_ui/src/app/page/Entry.tsx
@@ -3,10 +3,9 @@ import { gen_path } from "../page";
 export function Entry() {
   return (
     <div class={"flex flex-col items-center"}>
-      <p>Tools:</p>
       <div>
         <a class={"underline"} href={gen_path("navigation_compass_solver")}>
-          Navigation Compass Solver
+          引航罗盘推演
         </a>
       </div>
     </div>
