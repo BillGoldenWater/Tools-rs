3386555b5e4c4b8bca0dd35abca269655cd2da9c
Golden_Water <2439577268@qq.com>
2023-05-07 22:45:42 +0800

    feat(ui): optimize the style of RingIndicator.tsx

--- tools_web_ui/src/app/page/navigationCompassSolver/NavigationCompass.tsx
+++ tools_web_ui/src/app/page/navigationCompassSolver/NavigationCompass.tsx
@@ -14,16 +14,31 @@ export function NavigationCompass(props: NavigationCompassProps) {
           "relative aspect-square h-48 rounded-[50%] bg-neutral-300 dark:bg-neutral-600"
         }
       >
-        <RingIndicator offset={0} current={props.innerCurrent} active={true} />
-        <RingIndicator offset={1} current={props.middleCurrent} active={true} />
-        <RingIndicator offset={2} current={props.outerCurrent} active={true} />
+        <RingIndicator
+          offset={0}
+          current={props.innerCurrent}
+          active={true}
+          isInner={true}
+        />
+        <RingIndicator
+          offset={1}
+          current={props.middleCurrent}
+          active={true}
+          isInner={true}
+        />
+        <RingIndicator
+          offset={2}
+          current={props.outerCurrent}
+          active={true}
+          isInner={true}
+        />
 
-        <RingIndicator offset={3} current={0} active={true} />
-        <RingIndicator offset={3} current={1} active={false} />
-        <RingIndicator offset={3} current={2} active={false} />
-        <RingIndicator offset={3} current={3} active={false} />
-        <RingIndicator offset={3} current={4} active={false} />
-        <RingIndicator offset={3} current={5} active={false} />
+        <RingIndicator offset={3} current={0} active={true} isInner={false} />
+        <RingIndicator offset={3} current={1} active={false} isInner={false} />
+        <RingIndicator offset={3} current={2} active={false} isInner={false} />
+        <RingIndicator offset={3} current={3} active={false} isInner={false} />
+        <RingIndicator offset={3} current={4} active={false} isInner={false} />
+        <RingIndicator offset={3} current={5} active={false} isInner={false} />
       </div>
     </div>
   );
--- tools_web_ui/src/app/page/navigationCompassSolver/navigationCompass/RingIndicator.tsx
+++ tools_web_ui/src/app/page/navigationCompassSolver/navigationCompass/RingIndicator.tsx
@@ -5,6 +5,7 @@ export interface RingIndicatorProps {
   offset: number;
   current: number;
   active: boolean;
+  isInner: boolean;
 }
 
 export function RingIndicator(props: RingIndicatorProps) {
@@ -23,14 +24,17 @@ export function RingIndicator(props: RingIndicatorProps) {
           }),
         }}
         class={"group absolute left-24 top-24 h-0 w-0 transform-gpu"}
-        classList={{ ringIndicatorNonActive: !props.active }}
+        classList={{
+          ringIndicatorNonActive: !props.active,
+          ringIndicatorIsInner: props.isInner,
+        }}
       >
         <div
           style={{
             "--tw-translate-x": `${xOffset()}rem`,
           }}
           class={
-            "absolute h-2 w-8 -translate-y-1 transform-gpu rounded bg-cyan-400 group-[.ringIndicatorNonActive]:saturate-50 dark:bg-cyan-300 dark:group-[.ringIndicatorNonActive]:brightness-75"
+            "absolute h-2 w-8 -translate-y-1 transform-gpu rounded bg-cyan-400 group-[.ringIndicatorIsInner]:hue-rotate-[200deg] group-[.ringIndicatorNonActive]:saturate-50 dark:bg-cyan-300 dark:group-[.ringIndicatorNonActive]:brightness-75"
           }
         />
       </Motion.div>
