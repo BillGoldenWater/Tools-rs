b2f95bf848bc6aa29eb2b59d5656348cc3a78f77
Golden_Water <golden_water@chaosw.site>
2025-07-03 20:11:07 +0800

    feat(video_batch_compresser): load env from .env

--- video_batch_compresser/src/main.rs
+++ video_batch_compresser/src/main.rs
@@ -23,6 +23,7 @@ pub struct Args {
 }
 
 fn main() -> anyhow::Result<()> {
+    dotenvy::dotenv().ok();
     tracing_subscriber::fmt()
         .with_env_filter(EnvFilter::from_default_env())
         .init();
